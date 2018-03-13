834549a40e5a63921ed24ea511dcc17ed59459e6
JoshBlanks <jbblanks@yahoo.com>
2018-03-13 00:04:37 -0400

    Codility Tests

---
+++ PassingCars.java
@@ -0,0 +1,72 @@
+/*
+
+A non-empty zero-indexed array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+
+Array A contains only 0s and/or 1s:
+
+0 represents a car traveling east,
+1 represents a car traveling west.
+The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+For example, consider array A such that:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a non-empty zero-indexed array A of N integers, returns the number of pairs of passing cars.
+
+The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+For example, given:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+the function should return 5, as explained above.
+
+Assume that:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer that can have one of the following values: 0, 1.
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+
+*/
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A) {
+
+        // 0: Car -> East
+        // 1: West <- Car
+        // Once a car has passed, you don't add that in the following pairs
+
+        int pairs = 0;
+        int east = 0;
+
+        for(int i = 0; i < A.length; i++) {
+            if(A[i] == 0) { // count the number of 1's on the right
+                east++; // Make as many pairs as there are going east
+            } else { // car is going west, time to pair with number of cars east at this time
+                pairs += east;
+            }
+
+            if(pairs > 1000000000) return -1;
+        }
+
+        return pairs;
+    }
+}
